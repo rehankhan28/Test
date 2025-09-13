25d65135c99b2000769445974edb29495645c673
unknown <Rehan>
2025-09-13 21:02:31 +0530

    Initial commit - add my application

--- src/prep/array/DuplicateNum.java
+++ src/prep/array/DuplicateNum.java
@@ -1,11 +1,8 @@
 package prep.array;
 
-import java.util.Arrays;
 
 public class DuplicateNum {
     public static void main(String[] args) {
         int a[] = {11, 22, 22, 33, 445};
-        int min = Arrays.stream(a).min().getAsInt();
-        System.out.println(min);
     }
 }
