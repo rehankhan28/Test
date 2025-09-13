19bfc933988adf8ccc1e124504548d3d8087d3a5
unknown <Rehan>
2025-09-13 21:04:16 +0530

    First Change

--- src/prep/array/DuplicateNum.java
+++ src/prep/array/DuplicateNum.java
@@ -1,8 +1,12 @@
 package prep.array;
 
 
+import java.util.Arrays;
+
 public class DuplicateNum {
     public static void main(String[] args) {
         int a[] = {11, 22, 22, 33, 445};
+        int max = Arrays.stream(a).min().getAsInt();
+        System.out.println(max);
     }
 }
