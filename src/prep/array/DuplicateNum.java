54a0f6cc10f9d53feaa21dc7e7041a0839d32947
unknown <Rehan>
2025-09-13 18:47:43 +0530

    Describe your change

--- src/prep/array/DuplicateNum.java
+++ src/prep/array/DuplicateNum.java
@@ -2,6 +2,13 @@ package prep.array;
 
 public class DuplicateNum {
     public static void main(String[] args) {
-        int a[] = {};
+        int a[] = {11, 22, 22, 33, 445};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    System.out.println(a[i]);
+                }
+            }
+        }
     }
 }
