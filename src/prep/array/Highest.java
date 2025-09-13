ce6656b0cc7339f49354f46579505b4243cefaaa
unknown <Rehan>
2025-09-13 21:17:40 +0530

    First Comment

--- src/prep/array/Highest.java
+++ src/prep/array/Highest.java
@@ -5,7 +5,5 @@ import java.util.Arrays;
 public class Highest {
     public static void main(String[] args) {
         int a[] = {12, 21, 12, 34, 45, 56, 76};
-        int max = Arrays.stream(a).max().getAsInt();
-        System.out.println("Max: " + max);
     }
 }
