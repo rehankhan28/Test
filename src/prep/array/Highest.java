7bbf12ed31993decdd9c144e134e153ccbca3f32
unknown <Rehan>
2025-09-13 21:19:24 +0530

    Change Code

--- src/prep/array/Highest.java
+++ src/prep/array/Highest.java
@@ -5,5 +5,7 @@ import java.util.Arrays;
 public class Highest {
     public static void main(String[] args) {
         int a[] = {12, 21, 12, 34, 45, 56, 76};
+        int add = Arrays.stream(a).sum();
+        System.out.println(add);
     }
 }
